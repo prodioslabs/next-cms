5be54d0f0a68b243aed291402f77c55e0d6fa2f6
Abinash Panda <abinash@prodios.com>
2023-06-27 22:32:13 +0530

    feat(core): fix invalid cms data

--- src/core/collection.ts
+++ src/core/collection.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import fs from 'fs/promises'
 import { z } from 'zod'
 import { Field, InferFieldDataType, InferFieldZodSchema, getValidationSchemaForField } from './field'
-import { readData } from './data'
+import { fixData, generateDummyData } from './data'
 import { isErrnoException } from './utils/file'
 
 export type Collection = {
@@ -13,15 +13,11 @@ export type Collection = {
   fields: { [key: string]: Field }
 }
 
+/**
+ * Singleton is a collection with only one item. Right now there is no difference between
+ * collection and singleton, but in the future we might add some special behavior for singletons.
+ */
 export type Singleton = Collection
-
-type Optional<T> = T | undefined
-
-export type CollectionData<C extends Collection> = {
-  [FieldKey in keyof C['fields']]: C['fields'][FieldKey]['required'] extends true
-    ? InferFieldDataType<C['fields'][FieldKey]>
-    : Optional<InferFieldDataType<C['fields'][FieldKey]>>
-}
 
 export type ZodSchemaForCollection<C extends Collection> = z.ZodObject<{
   [FieldKey in keyof C['fields']]: C['fields'][FieldKey]['required'] extends true
@@ -30,13 +26,58 @@ export type ZodSchemaForCollection<C extends Collection> = z.ZodObject<{
 }>
 
 /**
+ * Creates a zod validation schema for the collection fields. This component is helper utility
+ * which can be used for creating validation schema for both collection and singletons
+ *
+ * @param collection collection to get the validation schema for
+ * @returns zod validation schema for the collection
+ * @see getValidationSchemaForCollection
+ * @see getValidationSchemaForSingleton
+ */
+function getValidationSchemaForFields<C extends Collection>(collection: Collection): ZodSchemaForCollection<C> {
+  let schema = z.object({})
+  Object.entries(collection.fields).forEach(([fieldKey, field]) => {
+    const fieldSchema = getValidationSchemaForField(field)
+    if (!field.required) {
+      schema = schema.extend({ [fieldKey]: fieldSchema.optional() })
+    } else {
+      schema = schema.extend({ [fieldKey]: fieldSchema })
+    }
+  })
+  return schema as ZodSchemaForCollection<C>
+}
+
+/**
+ * Create a zod validation schema for the collection. As the collection data would be
+ * present in an array, this function will create a zod array schema from the fields validation schema.
+ *
+ * @param collection collection to get the validation schema for
+ * @returns zod validation schema for the collection
+ * @see getValidationSchemaForFields
+ */
+function getValidationSchemaForCollection(collection: Collection) {
+  return z.array(getValidationSchemaForFields(collection))
+}
+
+/**
+ * Create a zod validation schema for the singleton. As the singleton data would be a single object,
+ * this function will create a zod object schema from the fields validation schema.
+ *
+ * @param singleton singleton to get the validation schema for
+ * @returns zod validation schema for the singleton
+ */
+function getValidationSchemaForSingleton(singleton: Singleton) {
+  return getValidationSchemaForFields(singleton)
+}
+
+/**
  * Get the collection folder path. If the folder is not present, it will create it.
  *
  * @param basePath base path of the project
  * @param collectionPath path of the collection
  * @returns the full path of the collection folder
  */
-async function getCollectionFolder(basePath: string, collectionPath: string) {
+async function getDataFolder(basePath: string, collectionPath: string) {
   const fullPath = path.resolve(basePath, collectionPath)
   try {
     await fs.stat(fullPath)
@@ -51,56 +92,104 @@ async function getCollectionFolder(basePath: string, collectionPath: string) {
 }
 
 /**
- * Creates a zod validation schema for the collection fields. This component is helper utility
- * which can be used for creating validation schema for both collection and singletons
+ * Read the collection data from the file. If the file is not present, it will create it using
+ * dummy data.
  *
- * @param collection collection to get the validation schema for
- * @returns zod validation schema for the collection
- * @see getValidationSchemaForCollection
- * @see getValidationSchemaForSingleton
+ * TODO: Break this function into smaller functions or figure out a better name for it
+ *
+ * @param dataPath complete path to the data file
+ * @param schema collection schema
+ * @param isSingleton is the data is fetched for a singleton or collection
+ * @returns
  */
-function _getValidationSchemaForCollectionFields(collection: Collection) {
-  let schema = z.object({})
-  Object.entries(collection.fields).forEach(([fieldKey, field]) => {
-    const fieldSchema = getValidationSchemaForField(field)
-    if (!field.required) {
-      schema = schema.extend({ [fieldKey]: fieldSchema.optional() })
+async function readOrCreateDataFromFile(dataPath: string, schema: Collection, isSingleton: boolean) {
+  try {
+    const data = await fs.readFile(dataPath, 'utf-8')
+    return JSON.parse(data)
+  } catch (error) {
+    if (isErrnoException(error) && error.code === 'ENOENT') {
+      const data = isSingleton ? generateDummyData(schema) : [generateDummyData(schema)]
+      await fs.writeFile(dataPath, JSON.stringify(data, null, 2), 'utf-8')
+      return data
     } else {
-      schema = schema.extend({ [fieldKey]: fieldSchema })
+      throw error
     }
-  })
-  return schema
+  }
 }
 
-function getValidationSchemaForCollection(collection: Collection) {
-  return z.array(_getValidationSchemaForCollectionFields(collection))
+/**
+ * Write back data to the file. This function will be used to write back the fixed data to the file.
+ *
+ * @param dataPath complete path to the data file
+ * @param data data to be written
+ * @see getCollectionData
+ * @see getSingletonData
+ */
+async function writeDataToFile(dataPath: string, data: any) {
+  await fs.writeFile(dataPath, JSON.stringify(data, null, 2), 'utf-8')
 }
 
-function getValidationSchemaForSingleton(collection: Collection) {
-  return _getValidationSchemaForCollectionFields(collection)
+type Optional<T> = T | undefined
+
+export type CollectionData<C extends Collection> = {
+  [FieldKey in keyof C['fields']]: C['fields'][FieldKey]['required'] extends true
+    ? InferFieldDataType<C['fields'][FieldKey]>
+    : Optional<InferFieldDataType<C['fields'][FieldKey]>>
 }
 
+/**
+ * Get the collection data from the local storage. If the data is not present, it will create it with dummy data.
+ * This method would also validate the data against the collection schema.
+ * If the validation fails, it will try to fix the data and return the fixed data.
+ *
+ * @param collection collection to get the data for
+ * @param basePath base path of the local storage folder
+ * @returns collection data
+ */
 export async function getCollectionData<C extends Collection>(collection: C, basePath = `${process.cwd()}/content`) {
-  const collectionFolderPath = await getCollectionFolder(basePath, collection.path)
+  const collectionFolderPath = await getDataFolder(basePath, collection.path)
   const collectionDataFile = path.resolve(collectionFolderPath, 'data.json')
-  const collectionData = await readData(collectionDataFile, collection, false)
+  const collectionData = await readOrCreateDataFromFile(collectionDataFile, collection, false)
 
   const validationSchema = getValidationSchemaForCollection(collection)
   try {
-    const parsedData = validationSchema.parse(collectionData) as CollectionData<C>[]
+    const parsedData = validationSchema.parse(collectionData)
     return parsedData
-  } catch (error) {}
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      const fixedData = fixData(collection, collectionData, error)
+      await writeDataToFile(collectionDataFile, fixedData)
+      return fixedData
+    }
+    throw error
+  }
 }
 
+/**
+ * Get the singleton data from the local storage. If the data is not present, it will create it with dummy data.
+ * This method would also validate the data against the singleton schema.
+ * If the validation fails, it will try to fix the data and return the fixed data.
+ *
+ * @param singleton singleton to get the data for
+ * @param basePath base path of the local storage folder
+ * @returns singleton data
+ */
 export async function getSingletonData<S extends Singleton>(singleton: S, basePath = `${process.cwd()}/content`) {
-  const singleFolderPath = await getCollectionFolder(basePath, singleton.path)
+  const singleFolderPath = await getDataFolder(basePath, singleton.path)
   const singletonDataFile = path.resolve(singleFolderPath, 'data.json')
-  const singletonData = await readData(singletonDataFile, singleton, true)
+  const singletonData = await readOrCreateDataFromFile(singletonDataFile, singleton, true)
 
   const validationSchema = getValidationSchemaForSingleton(singleton)
 
   try {
     const parsedData = validationSchema.parse(singletonData) as CollectionData<S>
     return parsedData
-  } catch (error) {}
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      const fixedData = fixData(singleton, singletonData, error)
+      await writeDataToFile(singletonDataFile, fixedData)
+      return fixedData
+    }
+    throw error
+  }
 }
--- src/core/data.ts
+++ src/core/data.ts
@@ -1,76 +1,88 @@
-import * as fs from 'fs/promises'
 import { z } from 'zod'
 import { faker } from '@faker-js/faker'
-import { Collection, CollectionData, ZodSchemaForCollection } from './collection'
-import { isErrnoException } from './utils/file'
+import { Collection, CollectionData } from './collection'
+import { Field } from './field'
 
-function generateDummyData<C extends Collection>(collection: C) {
+/**
+ * Generate dummy data for a field. This function will generate dummy data based on the
+ * field type and the default value if present. If default value is not present, it will
+ * use faker to generate the dummy data.
+ *
+ * @param field field for which the dummy data will be generated
+ * @returns dummy data
+ */
+function generateDummyDataForField(field: Field) {
+  if ('default' in field) {
+    return field.default
+  }
+
+  switch (field.type) {
+    case 'text':
+      return faker.lorem.sentence()
+
+    case 'rich-text':
+      return faker.lorem.paragraphs()
+
+    case 'number':
+      return faker.datatype.number()
+
+    case 'date':
+      return faker.date.past().toISOString()
+
+    case 'image':
+      return {
+        url: faker.image.urlLoremFlickr({ width: 640, height: 480, category: 'nature' }),
+        width: 640,
+        height: 480,
+      }
+
+    default:
+      throw new Error('Unknown field type')
+  }
+}
+
+export function generateDummyData<C extends Collection>(collection: C) {
   const data: Record<string, any> = {}
 
   Object.entries(collection.fields).forEach(([fieldKey, field]) => {
-    if ('default' in field) {
-      data[fieldKey] = field.default
-      return
-    }
-
-    switch (field.type) {
-      case 'text':
-        data[fieldKey] = faker.lorem.sentence()
-        break
-      case 'rich-text':
-        data[fieldKey] = faker.lorem.paragraphs()
-        break
-      case 'number':
-        data[fieldKey] = faker.datatype.number()
-        break
-      case 'date':
-        data[fieldKey] = faker.date.past().toISOString()
-        break
-      case 'image':
-        data[fieldKey] = {
-          url: faker.image.urlLoremFlickr({ width: 640, height: 480, category: 'nature' }),
-          width: 640,
-          height: 480,
-        }
-        break
-      default:
-        throw new Error('Unknown field type')
-    }
+    data[fieldKey] = generateDummyDataForField(field)
   })
 
   return data as CollectionData<C>
 }
 
-export async function readData(collectionDataPath: string, collectionSchema: Collection, isSingleton: boolean) {
-  try {
-    const data = await fs.readFile(collectionDataPath, 'utf-8')
-    return JSON.parse(data)
-  } catch (error) {
-    if (isErrnoException(error) && error.code === 'ENOENT') {
-      const data = isSingleton ? generateDummyData(collectionSchema) : [generateDummyData(collectionSchema)]
-      await fs.writeFile(collectionDataPath, JSON.stringify(data, null, 2), 'utf-8')
-      return data
-    } else {
-      throw error
-    }
+/**
+ * Fix invalid data based on the zod error
+ *
+ * @param invalidData invalid data to be fixed
+ * @param error zod error, based on which the data will be fixed
+ * @returns valid data
+ */
+export function fixData<C extends Collection>(schema: C, invalidData: Partial<CollectionData<C>>, error: z.ZodError) {
+  /**
+   * Fix the invalid item data based on the zod error. This is the helper function for
+   * fixing the error in the item.
+   *
+   * @param itemData item data to be fixed
+   * @param issuePath path to the issue in the item data
+   */
+  function fixItemData(itemData: any, issuePath: (string | number)[]) {
+    const fieldKeyWithIssue = issuePath[0]
+    const field = schema.fields[fieldKeyWithIssue]
+    const dummyData = generateDummyDataForField(field)
+    itemData[fieldKeyWithIssue] = dummyData
   }
-}
 
-function fixData<C extends Collection>(
-  invalidData: Partial<CollectionData<C>>,
-  validationSchema: ZodSchemaForCollection<C>,
-) {
-  try {
-    validationSchema.parse(invalidData)
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      const fixedData = { ...invalidData }
-      error.issues.forEach((issue) => {
-        const issuePath = issue.path
-      })
-      return fixedData
+  const fixedData = Array.isArray(invalidData) ? [...invalidData] : { ...invalidData }
+  error.issues.forEach((issue) => {
+    const issuePath = issue.path
+    // if the first element of the path is a number, then the issue must be in an
+    // item of an array
+    if (typeof issuePath[0] === 'number' && Array.isArray(fixedData)) {
+      fixItemData(fixedData[issuePath[0]], issuePath.slice(1))
     } else {
-      throw error
+      fixItemData(fixedData, issuePath)
     }
-  }
+  })
+  return fixedData as CollectionData<C>
 }
