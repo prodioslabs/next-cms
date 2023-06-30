f543eeb9025a78807220a33caca8299d28f39491
Abinash Panda <abinash@prodios.com>
2023-06-30 11:58:47 +0530

    feat(core): use @floating-ui for calculating positions

--- src/components/editable-link/editable-link.tsx
+++ src/components/editable-link/editable-link.tsx
@@ -1,7 +1,8 @@
 'use client'
 
+import { ComputePositionReturn, autoPlacement, autoUpdate, computePosition, offset } from '@floating-ui/dom'
 import Link from 'next/link'
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
 import { LuChevronRight } from 'react-icons/lu'
 import { Collection } from '~/core'
@@ -15,9 +16,40 @@ type EditableLinkProps = {
 
 export default function EditableLink({ collectionId, collection, elementId }: EditableLinkProps) {
   const [visible, setVisible] = useState(false)
+  const [computedPosition, setComputedPosition] = useState<ComputePositionReturn | undefined>(undefined)
+  const [containerBBox, setContainerBBox] = useState<DOMRect | undefined>()
 
-  // container element bounding box
-  const [containerBB, setContainerBB] = useState<DOMRect | undefined>(undefined)
+  useEffect(
+    function computeFloatingPositionOnVisiblityChange() {
+      let cleanup: () => void
+
+      if (visible) {
+        const element = document.getElementById(elementId)
+        const bboxElement = document.getElementById(`bbox-${elementId}`)
+        if (element && bboxElement) {
+          cleanup = autoUpdate(element, bboxElement, () => {
+            computePosition(element, bboxElement, {
+              middleware: [
+                autoPlacement({
+                  alignment: 'start',
+                }),
+                offset(6),
+              ],
+              strategy: 'fixed',
+            }).then(setComputedPosition)
+            setContainerBBox(element.getBoundingClientRect())
+          })
+        }
+      }
+
+      return () => {
+        if (cleanup) {
+          cleanup()
+        }
+      }
+    },
+    [visible, elementId],
+  )
 
   // timeout to prevent hiding the element as soon as mouse leaves the container element
   // instead of setting the visibility to false immediately, a setTimeout (with 100ms timeout)
@@ -30,14 +62,12 @@ export default function EditableLink({ collectionId, collection, elementId }: Ed
       clearTimeout(timeout.current)
     }
 
-    setContainerBB(document.getElementById(elementId)?.getBoundingClientRect())
     setVisible(true)
-  }, [elementId])
+  }, [])
 
   const handleMouseLeave = useCallback(() => {
     timeout.current = setTimeout(() => {
       setVisible(false)
-      setContainerBB(undefined)
     }, 200)
   }, [])
 
@@ -55,62 +85,36 @@ export default function EditableLink({ collectionId, collection, elementId }: Ed
     [elementId, handleMouseOver, handleMouseLeave],
   )
 
-  const styles = useMemo(() => {
-    if (!containerBB) {
-      return undefined
-    }
-
-    const topOffset = 64
-    const spaceAvailableAtTop = containerBB.top >= topOffset
-
-    return {
-      verticalPosition: spaceAvailableAtTop ? 'top' : 'bottom',
-    }
-  }, [containerBB])
-
-  return visible && containerBB
+  return visible
     ? createPortal(
         <>
-          {/* element rendering the border box */}
-          <div
-            className={cn('pointer-events-none fixed rounded-md border border-border', {
-              'rounded-tr-none': styles?.verticalPosition === 'top',
-              'rounded-br-none': styles?.verticalPosition === 'bottom',
-            })}
-            style={{
-              top: containerBB.top - 4,
-              left: containerBB.left - 4,
-              width: containerBB.width + 8,
-              height: containerBB.height + 8,
-            }}
-          />
           <Link
             href={`/admin/${collectionId}`}
-            className={cn('fixed z-10 flex transform justify-end', {
-              'translate-y-[calc(-100%-4px)]': styles?.verticalPosition === 'top',
-              'translate-y-[4px]': styles?.verticalPosition === 'bottom',
-            })}
+            className={cn(
+              'fixed flex w-max items-center whitespace-nowrap rounded border border-border bg-muted px-2 py-1 text-xs text-muted-foreground hover:bg-secondary hover:text-secondary-foreground',
+            )}
+            id={`bbox-${elementId}`}
             style={{
-              top: styles?.verticalPosition === 'top' ? containerBB.top : containerBB.bottom,
-              left: containerBB.left - 4,
-              width: containerBB.width + 8,
+              top: computedPosition?.y,
+              left: computedPosition?.x,
             }}
-            onMouseEnter={handleMouseOver}
+            onMouseOver={handleMouseOver}
             onMouseLeave={handleMouseLeave}
           >
-            <span
-              className={cn(
-                'flex flex-shrink-0 items-center whitespace-nowrap rounded border border-border bg-muted px-2 py-1 text-xs text-muted-foreground',
-                {
-                  'rounded-b-none border-b-0': styles?.verticalPosition === 'top',
-                  'rounded-t-none border-t-0': styles?.verticalPosition === 'bottom',
-                },
-              )}
-            >
-              Edit {collection.name}
-              <LuChevronRight className="ml-2 h-4 w-4" />
-            </span>
+            Edit {collection.name}
+            <LuChevronRight className="ml-2 h-4 w-4" />
           </Link>
+          {containerBBox ? (
+            <div
+              className="pointer-events-none fixed rounded-md border"
+              style={{
+                top: containerBBox.top - 4,
+                left: containerBBox.left - 4,
+                width: containerBBox.width + 8,
+                height: containerBBox.height + 8,
+              }}
+            />
+          ) : null}
         </>,
         document.getElementById('editable-element-portal')!,
       )
