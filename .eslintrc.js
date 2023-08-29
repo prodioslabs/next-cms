995455e84c58eda5876b0e970c942d51e25254f9
Abinash Panda <abinash@prodios.com>
2023-08-29 12:14:16 +0530

    feat(eslint): create a global eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  root: true,
+  // This tells ESLint to load the config from the package `eslint-config-custom`
+  extends: ['custom'],
+  settings: {
+    next: {
+      rootDir: ['apps/*/'],
+    },
+  },
+}
+
+module.exports = config
