5766f2308fd1caaa23fa7173e335191c450e4d54
Trugamr <trugamr@live.in>
2023-09-15 19:00:04 +0530

    fix(cms): use standard response instead of next response

--- packages/cms/src/lib/api.ts
+++ packages/cms/src/lib/api.ts
@@ -1,11 +1,10 @@
-import { NextResponse } from 'next/server'
 import { z } from 'zod'
 
 export function handleError(error: any) {
   if (error instanceof z.ZodError) {
-    return NextResponse.json({ message: error.message, issues: error.issues }, { status: 422 })
+    return Response.json({ message: error.message, issues: error.issues }, { status: 422 })
   } else if (error instanceof Error) {
-    return NextResponse.json({ message: error.message }, { status: 500 })
+    return Response.json({ message: error.message }, { status: 500 })
   }
-  return NextResponse.error()
+  return Response.error()
 }
