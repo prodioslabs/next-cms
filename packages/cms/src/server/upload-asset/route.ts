85c1212db4a76d98ba0ccb74d92aca2d2c723ec3
Trugamr <trugamr@live.in>
2023-09-15 15:33:52 +0530

    fix(cms): uploaded asset url generation

--- packages/cms/src/server/upload-asset/route.ts
+++ packages/cms/src/server/upload-asset/route.ts
@@ -2,10 +2,14 @@ import fs from 'fs/promises'
 import path from 'path'
 import imageSize from 'image-size'
 import { format } from 'date-fns'
-import { NextResponse } from 'next/server'
 import { isErrnoException } from '../../lib/file'
 import { uploadAssetBodySchema } from './schema'
 import { handleError } from '../../lib/api'
+
+function getRelativeAssetUrl(basePath: string, filePath: string) {
+  const assetUrl = path.relative(basePath, filePath)
+  return `/${path.posix.join(...assetUrl.split(path.sep))}`
+}
 
 async function getUploadDirectory(basePath: string, assetType: 'image' | 'video') {
   const currentDate = format(new Date(), 'dd-MM-yyyy')
@@ -30,32 +34,33 @@ export async function uploadAssetHandler(request: Request) {
       assetType: formData.get('assetType'),
     })
 
-    const basePath = `${process.cwd()}/public`
+    const basePath = path.join(process.cwd(), 'public')
     const uploadDirectory = await getUploadDirectory(basePath, assetType)
 
     switch (assetType) {
       case 'image': {
         const fileName = file.name
         const fileBuffer = Buffer.from(await file.arrayBuffer())
-        const { width, height, type } = await imageSize(fileBuffer)
+        const { width, height, type } = imageSize(fileBuffer)
+        const fileNameWithDimensions = `${width}x${height}_${fileName}`
 
-        const filepathWithDimensions = `${width}x${height}_${fileName}`
-        await fs.writeFile(path.resolve(uploadDirectory, filepathWithDimensions), fileBuffer)
+        const filePath = path.resolve(uploadDirectory, fileNameWithDimensions)
 
-        const assetUrl = path.resolve(uploadDirectory, filepathWithDimensions).replace(basePath, '')
+        const assetUrl = getRelativeAssetUrl(basePath, filePath)
 
-        return NextResponse.json({ url: assetUrl, width, height, type, assetType: 'image' }, { status: 200 })
+        return Response.json({ url: assetUrl, width, height, type, assetType: 'image' }, { status: 200 })
       }
 
       case 'video': {
         const fileName = file.name
         const fileBuffer = Buffer.from(await file.arrayBuffer())
 
+        const filePath = path.resolve(uploadDirectory, fileName)
         await fs.writeFile(path.resolve(uploadDirectory, fileName), fileBuffer)
 
-        const assetUrl = path.resolve(uploadDirectory, fileName).replace(basePath, '')
+        const assetUrl = getRelativeAssetUrl(basePath, filePath)
 
-        return NextResponse.json({ url: assetUrl, assetType: 'video' }, { status: 200 })
+        return Response.json({ url: assetUrl, assetType: 'video' }, { status: 200 })
       }
     }
   } catch (error) {
