670718b9e317b0a28feaf605d28a9fbbde30e10c
Trugamr <trugamr@live.in>
2023-09-19 15:19:26 +0530

    feat(cms): navigate to folder page on click

--- packages/cms/src/dashboard/components/admin-dashboard/components/file-card.tsx
+++ packages/cms/src/dashboard/components/admin-dashboard/components/file-card.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { File } from '@nextjs-cms/core'
-import { File as FileIcon } from 'lucide-react'
+import { File as FileIcon, ImageIcon, VideoIcon } from 'lucide-react'
+import { useMemo } from 'react'
 import {
   ContextMenu,
   ContextMenuContent,
@@ -20,14 +21,29 @@ type FileCardProps = {
 }
 
 export default function FileCard({ file, className, style }: FileCardProps) {
+  const icon = useMemo(() => {
+    const iconClassName = 'h-4 w-4 text-muted-foreground'
+
+    // TODO: Render image and video preview
+    switch (file.assetType) {
+      case 'image':
+        return <ImageIcon name="image" className={iconClassName} />
+      case 'video':
+        return <VideoIcon name="video" className={iconClassName} />
+      default:
+        return <FileIcon name="file" className={iconClassName} />
+    }
+  }, [file.assetType])
+
   return (
     <ContextMenu>
       <ContextMenuTrigger asChild>
         <button
           className={cn('flex items-center space-x-2 rounded-md border bg-background px-4 py-3 text-left', className)}
           style={style}
+          title={file.name}
         >
-          <FileIcon name="file" className="h-4 w-4 text-muted-foreground" />
+          {icon}
           <div className="flex-1 truncate text-sm text-foreground">{file.name}</div>
         </button>
       </ContextMenuTrigger>
--- packages/cms/src/dashboard/components/admin-dashboard/components/folder-card.tsx
+++ packages/cms/src/dashboard/components/admin-dashboard/components/folder-card.tsx
@@ -2,6 +2,7 @@
 
 import { Folder } from '@nextjs-cms/core'
 import { FolderOpen } from 'lucide-react'
+import Link from 'next/link'
 import {
   ContextMenu,
   ContextMenuContent,
@@ -23,13 +24,15 @@ export default function FolderCard({ folder, className, style }: FolderCardProps
   return (
     <ContextMenu>
       <ContextMenuTrigger asChild>
-        <button
+        <Link
           className={cn('flex items-center space-x-2 rounded-md border bg-background px-4 py-3 text-left', className)}
           style={style}
+          href={`/cms/admin/media-library/${folder.id}`}
+          title={folder.name}
         >
           <FolderOpen name="folder-open" className="h-4 w-4 text-muted-foreground" />
           <div className="flex-1 truncate text-sm text-foreground">{folder.name}</div>
-        </button>
+        </Link>
       </ContextMenuTrigger>
       <ContextMenuContent>
         <ContextMenuItem>Rename Folder</ContextMenuItem>
