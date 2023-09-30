cefbec71dc6185c9c22762ba5082ddde375e2d35
Trugamr <trugamr@live.in>
2023-09-30 15:36:37 +0530

    feat: add changesets for npm publishing

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
 const config = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ['custom'],
+  extends: ['@nextjs-cms/eslint-config-custom'],
   settings: {
     next: {
       rootDir: ['apps/*/'],
--- packages/cms/prettier.config.js
+++ packages/cms/prettier.config.js
@@ -1,7 +1,1 @@
-const config = require('prettier-config')
-// TODO: Remove this from prettier-config and add it to individual config
-// when required
-delete config.plugins
-delete config.tailwindFunctions
-
-module.exports = config
+module.exports = require('@nextjs-cms/prettier-config')
